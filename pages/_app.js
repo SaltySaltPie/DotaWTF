5833436e326c76f80a8ed1dffd255c48223df76a
SaltySaltPie <84831252+SaltySaltPie@users.noreply.github.com>
2021-11-04 12:36:05 +0700

    Update V4
    Remove Unused files
    Update icon and website title

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,15 @@ import Nav from "../src/components/nav/Nav";
 import useWindowSize from "../src/customHooks/useWindowSize";
 import Footer from "../src/components/footer/Footer";
 import "../styles/globals.scss";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   const windowSize = useWindowSize();
   return (
     <section className="body-container">
+      <Head>
+        <title>DotaWatafak</title>
+        <link rel="shortcut icon" href="logo.jpg"  />
+      </Head>
       {windowSize.width > 600 ? <LBanner /> : null}
       <section className="content-container">
         <Nav />
--- src/src/components/SocialMedia.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SocialMedia() {
-    return (
-        <section className='social-media-container'>
-            
-        </section>
-    )
-}
