0ec4bf86470522a9eb2e586c41276610f8d62c8c
SaltySaltPie <84831252+SaltySaltPie@users.noreply.github.com>
2021-11-04 09:06:31 +0700

    temp fix

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,18 @@ import dummyData from "../src/components/data/data";
 export async function getStaticProps() {
   try {
     const res = await fetch(
-      "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDlroSVtQOqgMgAVM5kM8d7q5TRKPHggcU&channelId=UC18NaGQLruOEw65eQE3bNbg&part=snippet,id&order=date&maxResults=20"
+      "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDlroSVtQOqgMgAVM5kM8d7q5TRKPHggcU&channelId=UC18NaGQLruOEw65eQE3bNbg&part=snippet,id&order=date&maxResults=3"
     );
     const data = await res.json();
     if (data.error) {
-        return{
-            props: {data: {items: dummyData}}
-        }
+      return {
+        props: { data: { items: dummyData } },
+        revalidate: 3600,
+      };
     }
     return {
       props: { data: data },
+      revalidate: 3600,
     };
   } catch (error) {
     console.log(error);
--- pages/news.js
+++
@@ -1,9 +0,0 @@
-function news() {
-  return (
-    <div>
-      <h1>Test News2</h1>
-    </div>
-  );
-}
-
-export default news;
